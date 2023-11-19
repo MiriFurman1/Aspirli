d24a58d8ee105b31a50b03b8557806cdf96bb860
Miri <mirifurman@gmail.com>
2023-11-19 14:57:36 +0200

    basic navbar design

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,11 +1,18 @@
-
-
 function Navbar() {
-  return (
-    <div className="bg-mint w-screen h-3/6">
-    <img src="logo.png" alt="logo"></img>
-  </div>
-  )
+return (
+    <div className="bg-mint w-screen flex flex-row justify-evenly">
+        <img src="logo.png" alt="logo" width={150}></img>
+        <ul className="flex flex-row justify-center mt-10">
+            <li className="p-3">דף הבית</li>
+            <li className="p-3">איזור אישי מרצה</li>
+            <li className="p-3">איזור אישי סטודנט</li>
+            <li className="p-3">שירותים</li>
+            <li className="p-3">קורסים נוספים</li>
+            <li className="p-3">אודות</li>
+            <li className="p-3">צור קשר</li>
+        </ul>
+    </div>
+    );
 }
 
-export default Navbar
+export default Navbar;
