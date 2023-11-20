566edd2583d864a977d1dcd498b22f6fe7b51f68
Miri <mirifurman@gmail.com>
2023-11-20 12:09:14 +0200

    basic router +navbar links

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,26 @@
 import "./App.css";
+import { Route, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Courses from "./pages/Courses";
+import Services from "./pages/Services";
+import PersonalArea from "./pages/PersonalArea";
+
 function App() {
   return (
     <div className="w-screen h-screen">
     <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/courses" element={<Courses/>}/>
+        <Route path="/services" element={<Services/>}/>
+        <Route path="/Personalarea" element={<PersonalArea/>}/>
+      </Routes>
 
     </div>
   );
--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,16 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
 return (
     <div className="bg-mint w-screen flex flex-row justify-evenly">
         <img src="logo.png" alt="logo" width={150}></img>
-        <ul className="flex flex-row justify-center mt-10">
-            <li className="p-3">דף הבית</li>
-            <li className="p-3">איזור אישי מרצה</li>
-            <li className="p-3">איזור אישי סטודנט</li>
-            <li className="p-3">שירותים</li>
-            <li className="p-3">קורסים נוספים</li>
-            <li className="p-3">אודות</li>
-            <li className="p-3">צור קשר</li>
-        </ul>
+        <div className="flex flex-row justify-center mt-10">
+            <Link to="/" className="p-3">דף הבית</Link>
+            <Link to="Personalarea" className="p-3">איזור אישי</Link>
+            <Link to="/services" className="p-3">שירותים</Link>
+            <Link to="/courses" className="p-3">קורסים נוספים</Link>
+            <Link to="/about" className="p-3">אודות</Link>
+            <Link to="/contact" className="p-3">צור קשר</Link>
+        </div>
     </div>
     );
 }
