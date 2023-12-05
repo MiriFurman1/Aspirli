68e4627194f5a2f9ede025f3a31d83676ac40010
Miri <mirifurman@gmail.com>
2023-12-05 15:19:18 +0200

    navbar routing fixed

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,21 +1,35 @@
-import { Link } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 function Navbar() {
-return (
+  const navigate = useNavigate();
+
+  const handleSectionClick = (sectionId: string) => {
+    navigate(`/#${sectionId}`);
+    setTimeout(() => scrollToSection(sectionId), 0);
+  };
+
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
     <div className="bg-mint max-w-screen flex flex-row justify-evenly flex-wrap">
-        <img src="logo.png" alt="logo" width={150}></img>
-        <div className="flex flex-row justify-center mt-10">
-            <Link to="/" className="p-3">דף הבית</Link>
-            <Link to="Personalarea" className="p-3">איזור אישי</Link>
-            <Link to="/services" className="p-3">שירותים</Link>
-            <Link to="/courses" className="p-3">קורסים נוספים</Link>
-            <a href="#about" className="p-3">אודות</a>
-            <a href="#contact" className="p-3">צור קשר</a>
-            <Link to="/login" className="p-3">התחבר</Link>
-            <Link to="/Register" className="p-3"> הירשם</Link>
-        </div>
+      <img src="logo.png" alt="logo" width={150}></img>
+      <div className="flex flex-row justify-center items-center mt-10">
+        <NavLink to="/" className="p-3">דף הבית</NavLink>
+        <NavLink to="/Personalarea" className="p-3">איזור אישי</NavLink>
+        <NavLink to="/services" className="p-3">שירותים</NavLink>
+        <NavLink to="/courses" className="p-3">קורסים נוספים</NavLink>
+        <NavLink to="/login" className="p-3">התחבר</NavLink>
+        <NavLink to="/Register" className="p-3">הירשם</NavLink>
+        <button  onClick={() => handleSectionClick("about")} className="bg-transparent border-none text-black hover:text-blue-500">אודות</button>
+        <button onClick={() => handleSectionClick("contact")} className="bg-transparent border-none text-black hover:text-blue-500">צור קשר</button>
+      </div>
     </div>
-    );
+  );
 }
 
 export default Navbar;
